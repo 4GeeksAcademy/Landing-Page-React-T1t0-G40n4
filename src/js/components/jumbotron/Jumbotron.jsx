346fd6e8df47c9be4c4817c622dc70d5ac2b9c47
T1t0-G40n4 <titogaona84@gmail.com>
2025-04-20 15:33:41 +0000

    Landing Page React T1t0-G40n4

--- src/js/components/jumbotron/Jumbotron.jsx
+++ src/js/components/jumbotron/Jumbotron.jsx
@@ -2,10 +2,11 @@ import React from "react"
 import "./Jumbotron.css"
 const Jumbotron = () =>{
     return (
-        <div className="jumbotron jumbotron-fluid jumbotron-content">
+        <div className="jumbotron jumbotron-fluid jumbotron-content mb-4">
             <div className="container">
                 <h1 className="display-4">Fluid jumbotron</h1>
                 <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                <a href="#" className="btn btn-primary mb-4">Go somewhere</a>
             </div>
         </div>
 
